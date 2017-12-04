e8525aae325ca46a67643dfdec7bb26b0d3aacbd
faisalkhalidsi <faisal.khalid.si@gmail.com>
2017-12-04 19:50:44 +0700

    BUg Fixed

--- views/indohome-status-order/index.php
+++ views/indohome-status-order/index.php
@@ -22,7 +22,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'queued',
             'status',
             // 'date',
-             'time',
+            'time',
 //            ['class' => 'yii\grid\ActionColumn'],
         ],
     ]);
